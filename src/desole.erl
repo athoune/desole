6b317b415038190ed88dc39f585fa1cc3d6e7adf
Mathieu Lecarme <mathieu@garambrogne.net>
2012-03-25 21:34:57 +0200

    Compound.

--- src/desole.erl
+++ src/desole.erl
@@ -2,24 +2,41 @@
 
 -export([run/3, auto_type/1]).
 
-
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -export([functions/1]).
 -endif.
 
+-record(context, {
+    context,
+    methods
+    }).
+
 run(Context, Libs, Actions) ->
     run(Context, Libs, Actions, nil).
 
 run(Context, Libs, Actions, Result) ->
-    run_(Context, functions(Libs), Actions, Result).
+    one_line(#context{context = Context, methods=functions(Libs)}, Actions, Result).
 
-run_(Context, Methods, [{Action, Args}|Tail], _Result) ->
-    F = proplists:get_value(Action, Methods),
-    {ok, NewContext, NewResult} = F(Context, Args),
-    run_(NewContext, Methods, Tail, NewResult);
-run_(Context, _Methods, [], Result) ->
+one_line(Context, [Head|Tail], _Result) ->
+    {NewContext, NewResult} = eval(Context, Head),
+    one_line(NewContext, Tail, NewResult);
+one_line(Context, [], Result) ->
     {ok, Context, Result}.
+
+eval(#context{methods=Methods} = Context, {'fun', Action, Args}) ->
+    {NewContext, ArgsValue} = evals(Context, Args, []),
+    F = proplists:get_value(Action, Methods),
+    {ok, NewContext, NewResult} = F(Context, ArgsValue),
+    {NewContext, NewResult};
+eval(Context, Value) ->
+    {Context, Value}.
+
+evals(Context, [Var|Vars], Acc) ->
+    {NewContext, Value} = eval(Context, Var),
+    evals(NewContext, Vars, [ Value | Acc]);
+evals(Context, [], Acc) ->
+    {Context, Acc}.
 
 functions(Libs) ->
     lists:flatten(lists:foldl(fun(T, Acc) ->
--- test/simple_test.erl
+++ test/simple_test.erl
@@ -9,6 +9,17 @@ function_test() ->
         end, [], M)),
     ?assertEqual([add,remove,'if',ifelse,while,for], F).
 
+%% add(2, 1)
 add_test() ->
-    A = desole:run([], [desole_main], [{add, [{int, 2}, {int, 1}]}]),
-    ?assertEqual({ok, [], {int, 3}}, A).
+    {ok, _Context, A} = desole:run([], [desole_main], [{'fun', add, [{int, 2}, {int, 1}]}]),
+    ?assertEqual({int, 3}, A).
+
+%% remove(add(2, 1), 3)
+compose_test() ->
+    {ok, _Context, A} = desole:run([], [desole_main], [
+        {'fun',  remove, [
+            {'fun', add, [{int, 2}, {int, 1}]},
+            {int, 3}]
+        }]),
+    ?debugFmt("~p~n", [A]),
+    ?assertEqual({int, 0}, A).
