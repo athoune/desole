083f9c0ede46f16481bc957057be20686993efb2
Mathieu Lecarme <mlecarme@bearstech.com>
2012-03-27 11:51:06 +0200

    Simple function names.

--- src/desole_boolean.erl
+++ src/desole_boolean.erl
@@ -1,20 +1,20 @@
 -module(desole_boolean).
 
--export([or_/2, and_/2, not_/2]).
+-export(['or'/2, 'and'/2, 'not'/2]).
 -export([export/0]).
 -behaviour(desole_lib).
 
 export() -> [
-        {'or', fun or_/2},
-        {'and', fun and_/2},
-        {'not', fun not_/2}
+        {'or', fun 'or'/2},
+        {'and', fun 'and'/2},
+        {'not', fun 'not'/2}
     ].
 
-or_(Context, [{boolean, A}, {boolean, B}]) ->
+'or'(Context, [{boolean, A}, {boolean, B}]) ->
     {ok, Context, A or B}.
 
-and_(Context, [{boolean, A}, {boolean, B}]) ->
+'and'(Context, [{boolean, A}, {boolean, B}]) ->
     {ok, Context, A and B}.
 
-not_(Context, [{boolean, A}]) ->
+'not'(Context, [{boolean, A}]) ->
     {ok, Context, not(A)}.
